bcdb9a113206b8838e69415532e05b6abf96246f
Ranjeetch <99755760+Ranjeetch@users.noreply.github.com>
2022-03-31 10:48:35 +0530

    Quick Tap

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -14,6 +14,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
